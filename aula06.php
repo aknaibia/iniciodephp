531a187e40b8bf9af7407746b91478ac01a7e256
aknaibia <166161954+aknaibia@users.noreply.github.com>
2024-08-01 14:44:56 -0300

    carros

---
+++ aula06.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula 06 de PHP</title>
+</head>
+<body>
+    <?php 
+        echo "<h1>Arrays - Vetores<h1>";
+        $carros = array ('Fiesta','Palio', 'Corsa','Siena');
+        echo $carros[1],"<br>";
+        echo $carros[3],"<br>";
+
+    ?>
+</body>
+</html>
