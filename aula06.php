390d0d5df06e55ba0603ac16df3bf4fa882fd851
aknaibia <166161954+aknaibia@users.noreply.github.com>
2024-08-06 14:13:42 -0300

    Update aula06.php

--- aula06.php
+++ aula06.php
@@ -4,54 +4,58 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-<style>
-
-body
-{
-    
-    background-color: lightpink;
-}
-</style>
-
+    <style>
+        body {
+            background-color: lightpink;
+        }
+    </style>
 </head>
 <body>
 <?php
-echo "<h1> Arrays - Vetores </h1>";
-$carros = array ('Fiest','Palio','Corsa','Siena');
-echo $carros [1], "<br>";
-echo $carros [3],"<br>";
+echo "<h1>Arrays - Vetores</h1>";
+
+// Inicializando o array
+$carros = array('Fiesta', 'Palio', 'Corsa', 'Siena');
+
+// Acessando e exibindo elementos do array
+echo $carros[1], "<br>"; // Resultado: Palio
+echo $carros[3], "<br>"; // Resultado: Siena
+
 // Adicionando novos elementos
-$carros [4]= "Clio";
-$carros [11] = "Versa";
-$carros [ ] ="Sandero";
-echo $carros [4] , "<br>"; //Resultado Clio
-echo $carros [5] , "<br>"; //Sem resultado
-echo $carros [11] , "<br>"; //Resultado versa
-echo $carros [12] , "<br>"; // Reaultado Sandero
+$carros[4] = "Clio";
+$carros[11] = "Versa";
+$carros[5] = "Sandero"; // Corrigido para definir um valor no índice 5
 
-$carros ["s1"] = "Siena"; //usando strig como indice
-echo $carros ["s1"] , "<br>"; // Resultado Siena
-echo [13], "<br>";
-$carros [13] = "Ferrari";
-echo $carros[13], "<br>";
-$carros[5] = "fusca";
+// Exibindo elementos adicionados
+echo $carros[4], "<br>";  // Resultado: Clio
+echo $carros[5], "<br>";  // Resultado: Sandero
+echo $carros[11], "<br>"; // Resultado: Versa
+echo isset($carros[12]) ? $carros[12] . "<br>" : "Índice 12 não definido<br>"; // Índice 12 não foi definido
 
-echo"<h1>Impressão completa da Array</h1>";
- 
-echo "<pre>";
-print_r($carros);
- 
-echo"<h1>Impressão completa da Array</h1>";
+// Usando string como índice
+$carros["s1"] = "Siena";
+echo $carros["s1"], "<br>"; // Resultado: Siena
+
+// Adicionando outro elemento
+$carros[13] = "Ferrari";
+echo $carros[13], "<br>"; // Resultado: Ferrari
+
+// Alterando o valor de um elemento existente
+$carros[5] = "Fusca";
+echo $carros[5], "<br>"; // Resultado: Fusca
+
+// Impressão completa do array
+echo "<h1>Impressão completa do Array</h1>";
 
 echo "<pre>";
 print_r($carros);
 echo "</pre>";
 
+echo "<h1>Detalhamento do Array</h1>";
+
 echo "<pre>";
-var_dump ($carros);
+var_dump($carros);
 echo "</pre>";
-
-
-  ?>
+?>
 </body>
 </html>
